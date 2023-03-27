5da38f162a5bd8e71848936e58fe0a69ae95533d
hoc2000 <vutuhoc@gmail.com>
2023-03-27 14:21:56 +0700

    change delete a lots

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = 2
+DATABASES = 1
 
 if DEBUG and (DATABASES == 1):
     DATABASES = {
