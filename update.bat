f1fbf1de0e18501903467e754df38bc58b6ce0be
hoc2000 <vutuhoc@gmail.com>
2023-05-08 14:09:28 +0700

    delete file

--- update.bat
+++
@@ -1,7 +0,0 @@
-git fetch --all
-
-git checkout origin/master -- LMS/LMS
-git checkout origin/master -- LMS/static
-git checkout origin/master -- LMS/app
-git checkout origin/master -- LMS/templates
-git checkout origin/master -- LMS/Media/statics
