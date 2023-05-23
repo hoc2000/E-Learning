bba53c9547909c0ef4a3831138e84a2348a96996
hoc2000 <vutuhoc@gmail.com>
2023-05-23 10:26:03 +0700

    new file update

---
+++ update.cmd
@@ -0,0 +1,6 @@
+git fetch --all
+
+git checkout origin/master -- LMS/LMS
+git checkout origin/master -- LMS/static
+git checkout origin/master -- LMS/app
+git checkout origin/master -- LMS/templates
